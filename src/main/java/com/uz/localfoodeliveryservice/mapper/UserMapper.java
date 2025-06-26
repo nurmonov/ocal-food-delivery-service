a4f471693c78891fa29935303dcb0edc3984d6d1
sherali <nurmonoff@gmail.com>
2025-06-27 00:08:19 +0500

    mapper

--- src/main/java/com/uz/localfoodeliveryservice/mapper/UserMapper.java
+++ src/main/java/com/uz/localfoodeliveryservice/mapper/UserMapper.java
@@ -3,11 +3,14 @@ package com.uz.localfoodeliveryservice.mapper;
 import com.uz.localfoodeliveryservice.dto.request.UserUpdateRequest;
 import com.uz.localfoodeliveryservice.dto.response.UserResponse;
 import com.uz.localfoodeliveryservice.entity.User;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel="spring")
 public interface UserMapper {
     UserResponse userResponse(User user);
-    User userEntity( UserUpdateRequest userUpdateRequest);
-
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUserFromDto(UserUpdateRequest request, @MappingTarget User user);
 }
