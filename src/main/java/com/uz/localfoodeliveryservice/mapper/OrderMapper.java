ccfe1645f973d11eb9db4d5362b58bba22a2fc02
Javlon <javlonbekjadikov@gmail.com>
2025-06-26 20:56:12 +0500

    Service

--- src/main/java/com/uz/localfoodeliveryservice/mapper/OrderMapper.java
+++ src/main/java/com/uz/localfoodeliveryservice/mapper/OrderMapper.java
@@ -4,7 +4,7 @@ import com.uz.localfoodeliveryservice.dto.request.OrderRequest;
 import com.uz.localfoodeliveryservice.dto.response.OrderResponse;
 import com.uz.localfoodeliveryservice.entity.Order;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
